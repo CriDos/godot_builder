e47c15c5d07d4ce5afd125ac09e8d90b0f91441d
CriDos <CriDos@ya.ru>
2018-09-23 17:22:25 +0300

    fix

--- win64_mono/1_build_temporary_and_mono_glue generated.bat
+++ win64_mono/1_build_temporary_and_mono_glue generated.bat
@@ -2,7 +2,7 @@ call utils/set_sdk
 call utils/set_vs x64
 call utils/set_godot_dir
 
-call scons -j3 platform=windows tools=yes target=debug module_mono_enabled=yes mono_glue=no
-call "bin/godot.windows.opt.tools.64.mono.exe" --generate-mono-glue modules/mono/glue
+call scons -j3 platform=windows tools=yes module_mono_enabled=yes mono_glue=no
+call "bin/godot.windows.tools.64.mono.exe" --generate-mono-glue modules/mono/glue
 
 pause
