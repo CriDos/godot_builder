f644fd34d7ce43f3a6556ae2e3a99b72189ea0e7
CriDos <CriDos@ya.ru>
2019-02-12 04:52:39 +0300

    upd

---
+++ win64_mono/release/1_build_and_mono_glue_generated.bat
@@ -0,0 +1,8 @@
+call ../utils/set_sdk
+call ../utils/set_vs x64
+call ../utils/set_godot_dir
+
+call scons -j3 platform=windows target=release_debug tools=yes module_mono_enabled=yes mono_glue=no
+call "bin/godot.windows.opt.tools.64.mono.exe" --generate-mono-glue modules/mono/glue
+
+pause
--- win64_mono/release/2_build_editor.bat
+++ win64_mono/release/2_build_editor.bat
@@ -1,6 +1,6 @@
-call utils/set_sdk
-call utils/set_vs x64
-call utils/set_godot_dir
+call ../utils/set_sdk
+call ../utils/set_vs x64
+call ../utils/set_godot_dir
 
 call scons -j3 p=windows target=release_debug tools=yes module_mono_enabled=yes
 
