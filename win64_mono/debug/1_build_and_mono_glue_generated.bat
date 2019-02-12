5f47be85eb6f523e8ddc8a9bc088e208e8405dee
CriDos <CriDos@ya.ru>
2019-02-13 02:54:37 +0300

    upd

--- win64_mono/debug/1_build_and_mono_glue_generated.bat
+++ win64_mono/debug/1_build_and_mono_glue_generated.bat
@@ -2,7 +2,7 @@ call ../utils/set_sdk
 call ../utils/set_vs x64
 call ../utils/set_godot_dir
 
-call scons -j3 platform=windows target=debug tools=yes module_mono_enabled=yes mono_glue=no
+call scons -j3 platform=windows target=debug tools=yes module_mono_enabled=yes mono_glue=no vsproj=yes
 call "bin/godot.windows.tools.64.mono.exe" --generate-mono-glue modules/mono/glue
 
 pause
--- win64_mono/utils/install/dist/install_packages.bat
+++ win64_mono/utils/install/dist/install_packages.bat
@@ -2,4 +2,4 @@ python -m pip install --upgrade pip
 pip install -U setuptools
 pip install -U wheel
 pip install pywin32
-pip install SCons
+pip install SCons==3.0.1
