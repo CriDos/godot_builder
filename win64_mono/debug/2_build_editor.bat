bb75919a274282f60c0662a78d03c11c2805aa36
CriDos <CriDos@ya.ru>
2019-02-24 15:35:39 +0300

    upd

--- win64_mono/debug/2_build_editor.bat
+++ win64_mono/debug/2_build_editor.bat
@@ -2,6 +2,6 @@ call ../utils/set_sdk
 call ../utils/set_vs x64
 call ../utils/set_godot_dir
 
-call scons -j3 p=windows target=debug tools=yes module_mono_enabled=yes vsproj=yes
+call scons -j3 p=windows target=debug tools=yes module_mono_enabled=yes
 
 pause
